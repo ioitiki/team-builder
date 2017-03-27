801ced67ba1b1959fd57a2640b66ad298ade1f8c
Andrew Malkin <andymalkin@hotmail.com>
2017-03-27 14:02:53 -0700

    add better navigattion to the frontend

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,6 +18,12 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("/about", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/about-startup-event.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     get("/teams/new", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/team-form.vtl");
